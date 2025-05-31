fa8a303ff0c1ef2895c614d7ea1b54d4d3f54cae
Purbayan Chowdhury <pur.cho.99@gmail.com>
2025-05-31 13:44:36 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,12 +1,24 @@
 import sys
 
 
+def parse_command(command):
+    command_parts = command.split()
+
+    if len(command_parts) == 0:
+        return
+
+    if command_parts[0] == "exit":
+        sys.exit(int(command_parts[1]))
+
+    sys.stdout.write("{}: command not found\n".format(command))
+
+
 def main():
     # Uncomment this block to pass the first stage
     sys.stdout.write("$ ")
     # Wait for user input
     command = input()
-    sys.stdout.write("{}: command not found\n".format(command))
+    parse_command(command)
     main()
 
 
