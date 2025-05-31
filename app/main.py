672dda708fca06a36a135b71329f72934cf9070c
Purbayan Chowdhury <pur.cho.99@gmail.com>
2025-05-31 21:03:59 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,48 +2,19 @@ import sys
 import shutil
 import os
 from typing import Tuple, List
+import shlex
 
 
 def parse_arguments(command: str) -> Tuple[str, List[str]]:
-    command_parts = command.split(" ", maxsplit=1)
+    # command_parts = command.split(" ", maxsplit=1)
+    command_parts = shlex.split(command)
     cmd = command_parts[0]
     if len(command_parts) == 1:
         return (cmd, [])
 
-    quotes = ["'", '"']
-    command_parts = command_parts[-1]
-    if quotes[0] not in command_parts and quotes[1] not in command_parts:
-        return (cmd, list(filter(lambda x: x.strip() != "", command_parts.split(" "))))
+    args = command_parts[1:]
 
-    args = []
-    i = 0
-    while i < len(command_parts):
-        if command_parts[i] in quotes:
-            quote_idx = quotes.index(command_parts[i])
-            start_idx = i + 1
-            end_idx = command_parts.find(quotes[quote_idx], start_idx)
-            if end_idx == -1:
-                print("{}: unmatched quote".format(command))
-                return (cmd, [])
-            args.append(command_parts[start_idx:end_idx])
-
-            if (
-                end_idx != len(command_parts) - 1
-                and command_parts[end_idx + 1] == quotes[quote_idx]
-            ):
-                start_idx = end_idx + 2
-                end_idx = command_parts.find(quotes[quote_idx], start_idx + 1)
-                args[-1] += command_parts[start_idx:end_idx]
-        else:
-            start_idx = i
-            end_idx = command_parts.find(" ", start_idx)
-            if end_idx == -1:
-                args.append(command_parts[start_idx:])
-                break
-            args.append(command_parts[start_idx:end_idx])
-        i = end_idx + 1
-
-    args = list(filter(lambda x: x.strip() != "", args))
+    # args = list(filter(lambda x: x.strip() != "", args))
     # print(str(args), file=sys.stderr)
 
     return (cmd, args)
