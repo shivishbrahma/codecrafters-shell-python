653ea99749ac2138fea9f88e9e134315c2ce6731
Purbayan Chowdhury <pur.cho.99@gmail.com>
2025-05-31 16:16:01 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -12,8 +12,7 @@ def parse_command(command):
     commands_list = ["echo", "exit", "type", "pwd"]
 
     if command_parts[0] == "echo":
-        sys.stdout.write(" ".join(command_parts[1:]))
-        sys.stdout.write("\n")
+        sys.stdout.write(" ".join(command_parts[1:]) + "\n")
         return
 
     if command_parts[0] == "type":
@@ -40,11 +39,24 @@ def parse_command(command):
         sys.stdout.write("\n")
         return
 
+    if command_parts[0] == "cd":
+        if len(command_parts) > 1:
+            dir_path = os.path.expanduser(command_parts[1])
+            if os.path.exists(dir_path):
+                os.chdir(dir_path)
+            else:
+                sys.stdout.write(
+                    "{}: {}: No such file or directory\n".format(
+                        command_parts[0], command_parts[1]
+                    )
+                )
+            return
+
     if path := shutil.which(command_parts[0]):
         os.system(command)
         return
 
-    sys.stdout.write("{}: command not found\n".format(command))
+    sys.stdout.write("{}: command not found\n".format(command_parts[0]))
 
 
 def main():
